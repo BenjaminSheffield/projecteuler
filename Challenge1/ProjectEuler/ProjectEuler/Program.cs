507e3fa49b9e869c878b93e6be5cab4bfc5d01cf
Benjamin Sheffield <benjaminsheffield@live.com>
2015-06-02 18:46:17 +1200

    First challenge complete

---
+++ Challenge1/ProjectEuler/ProjectEuler/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MultipleSums(3, 5, 1000));
+            Console.ReadLine();
+        }
+
+        public static int MultipleSums(int firstNumber, int secondNumber, int total)
+        {
+            int totalNumber = 0;
+            for (int i = 0; i < total; i++)
+            {
+                if (i % firstNumber == 0)
+                {
+                    totalNumber += i;
+                }
+                if (i % secondNumber == 0 && i % firstNumber !=0)
+                {
+                    totalNumber += i;
+                }
+            }
+            return totalNumber;
+        }
+    }
+}

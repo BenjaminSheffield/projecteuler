7feaefdfc6bca2eda66cd9991fad89117e8590f2
Benjamin Sheffield <benjaminsheffield@live.com>
2015-06-02 20:36:31 +1200

    Refactoring C#, JavaScript, Ruby

--- Challenge1/JavaScript/Challenge1.js
+++ Challenge1/JavaScript/Challenge1.js
@@ -1,12 +1,9 @@
 function MultipleSums(firstNumber, secondNumber, total) {
     var totalNumber = 0;
     for (i = 0; i < total; i++) {
-        if (i % firstNumber == 0) {
+        if (i % firstNumber == 0 || i % secondNumber == 0 ) {
             totalNumber += i;
-        }
-        if (i % secondNumber == 0 && i % firstNumber != 0) {
-            totalNumber += i;
-        }
+        }        
     }
     return totalNumber;
 }
